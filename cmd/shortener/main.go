65c86d2b8978852633e55bd2b39f5cc62de728b6
pingachguk <imayakimov@gmail.com>
2023-09-30 16:00:12 +0300

    add

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,53 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+var urls map[string]string
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		_, id, _ := strings.Cut(r.RequestURI, "/")
+		url := getShort(id)
+		if len(url) == 0 {
+			http.NotFound(w, r)
+		} else {
+			http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+		}
+	case http.MethodPost:
+		b := make([]byte, r.ContentLength)
+		r.Body.Read(b)
+		short := createShort(string(b))
+		res := fmt.Sprintf("http://%s/%s", r.Host, short)
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(res))
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func getShort(id string) string {
+	return urls[id]
+}
+
+func createShort(url string) string {
+	res := fmt.Sprint(len(urls) + 1)
+	urls[res] = url
+
+	return res
+}
+
+func main() {
+	urls = make(map[string]string)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", mainHandler)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pingachguk/ya-shortener
+
+go 1.19
