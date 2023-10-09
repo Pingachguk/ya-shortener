997cfb187c698cea643802cf91f1ff7aa8b0461a
pingachguk <imayakimov@gmail.com>
2023-10-10 00:27:51 +0300

    Правим конфиг

--- config/config.go
+++ config/config.go
@@ -12,32 +12,61 @@ type Addr struct {
 	Port int
 }
 
+type SchemeAddr struct {
+	Addr
+	Scheme string
+}
+
 func (addr *Addr) Set(value string) error {
 	hp := strings.Split(value, ":")
-	if len(hp) != 2 {
-		return errors.New("need address in a form host:port")
+	if len(hp) == 2 {
+		port, err := strconv.Atoi(hp[1])
+		if err != nil {
+			return err
+		}
+		addr.Port = port
 	}
-	port, err := strconv.Atoi(hp[1])
-	if err != nil {
-		return err
-	}
+
 	addr.Host = hp[0]
-	addr.Port = port
 	return nil
 }
 
 func (addr *Addr) String() string {
-	return fmt.Sprintf("%s:%d", addr.Host, addr.Port)
+	if addr.Port != 0 {
+		return fmt.Sprintf("%s:%d", addr.Host, addr.Port)
+	} else {
+		return addr.Host
+	}
 }
 
-var defaultAddr Addr = Addr{
-	Host: "0.0.0.0",
-	Port: 8080,
+func (addr *SchemeAddr) Set(value string) error {
+	shp := strings.Split(value, "://")
+	if len(shp) != 2 {
+		return errors.New("need address in a form scheme://host:port")
+	}
+	addr.Addr.Set(shp[1])
+	addr.Scheme = shp[0]
+	return nil
+}
+
+func (addr *SchemeAddr) String() string {
+	return fmt.Sprintf("%s://%s", addr.Scheme, addr.Addr.String())
 }
 
 var (
+	defaultAddr Addr = Addr{
+		Host: "0.0.0.0",
+		Port: 8080,
+	}
+	schemeDefaultAddr SchemeAddr = SchemeAddr{
+		defaultAddr,
+		"http",
+	}
+)
+
+var (
 	AppAddr  = defaultAddr
-	BaseAddr = defaultAddr
+	BaseAddr = schemeDefaultAddr
 )
 
 func InitConfig() {
