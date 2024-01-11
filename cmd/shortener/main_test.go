b51353f175ced7ffda879201dcdc7fd99b7fb8ab
Андрей Якимов <yakimov.a@promail366.com>
2024-01-11 11:18:11 +0300

    fix: подливаем тест из iter8

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,6 +12,7 @@ import (
 
 	"github.com/go-resty/resty/v2"
 	"github.com/pingachguk/ya-shortener/config"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -182,6 +184,7 @@ func TestCompress(t *testing.T) {
 		req.SetHeader("Accept-Encoding", "gzip")
 		req.SetHeader("Content-Type", "application/json")
 		req.SetBody(requestBody)
+		req.SetDoNotParseResponse(true)
 
 		res, err := req.Send()
 
@@ -191,6 +194,13 @@ func TestCompress(t *testing.T) {
 		contentEncoding := res.Header().Get("Content-Encoding")
 		hasGzip := strings.Contains(contentEncoding, "gzip")
 		assert.True(t, hasGzip, "Заголовок Content-Encoding не содержит значение gzip")
+
+		zr, err := gzip.NewReader(res.RawResponse.Body)
+		require.NoError(t, err, "Ошибка инициализации gzip.Reader")
+
+		result, err := io.ReadAll(zr)
+		require.NoError(t, err, "Ошибка чтения gzip.Reader")
+		log.Info().Msgf("%s", string(result))
 	})
 }
 
